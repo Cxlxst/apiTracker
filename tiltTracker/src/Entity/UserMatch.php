b717cab98a028719e864f622bc98b0945fbe0693
MICLEA Christian <66925033+MicleaChristian@users.noreply.github.com>
2023-12-06 15:00:49 +0100

    update Bdd

--- tiltTracker/src/Entity/UserMatch.php
+++ tiltTracker/src/Entity/UserMatch.php
@@ -2,8 +2,8 @@
 
 namespace App\Entity;
 
-use App\Repository\UserMatchRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserMatchRepository;
 
 #[ORM\Entity(repositoryClass: UserMatchRepository::class)]
 class UserMatch
@@ -28,76 +28,77 @@ class UserMatch
     #[ORM\Column(nullable: true)]
     private ?int $nbMatchsPerdus = null;
 
-    #[ORM\OneToOne(mappedBy: 'idUser', cascade: ['persist', 'remove'])]
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'userMatches')]
+    #[ORM\JoinColumn(nullable: false)]
     private ?User $user = null;
 
-    public function getId(): ?int
-    {
+    #[ORM\OneToOne(mappedBy: 'userMatch', targetEntity: Partie::class, cascade: ['persist', 'remove'])]
+    private ?Partie $partie = null;
+
+    public function getId(): ?int {
         return $this->id;
     }
 
-    public function getFrags(): ?int
-    {
+    public function getFrags(): ?int {
         return $this->frags;
     }
 
-    public function setFrags(?int $frags): static
-    {
+    public function setFrags(?int $frags): self {
         $this->frags = $frags;
-
         return $this;
     }
 
-    public function getMorts(): ?int
-    {
+    public function getMorts(): ?int {
         return $this->morts;
     }
 
-    public function setMorts(?int $morts): static
-    {
+    public function setMorts(?int $morts): self {
         $this->morts = $morts;
-
         return $this;
     }
 
-    public function getAssists(): ?int
-    {
+    public function getAssists(): ?int {
         return $this->assists;
     }
 
-    public function setAssists(int $assists): static
-    {
+    public function setAssists(int $assists): self {
         $this->assists = $assists;
-
         return $this;
     }
 
-    public function getNbMatchsGagnes(): ?int
-    {
+    public function getNbMatchsGagnes(): ?int {
         return $this->nbMatchsGagnes;
     }
 
-    public function setNbMatchsGagnes(?int $nbMatchsGagnes): static
-    {
+    public function setNbMatchsGagnes(?int $nbMatchsGagnes): self {
         $this->nbMatchsGagnes = $nbMatchsGagnes;
-
         return $this;
     }
 
-    public function getNbMatchsPerdus(): ?int
-    {
+    public function getNbMatchsPerdus(): ?int {
         return $this->nbMatchsPerdus;
     }
 
-    public function setNbMatchsPerdus(?int $nbMatchsPerdus): static
-    {
+    public function setNbMatchsPerdus(?int $nbMatchsPerdus): self {
         $this->nbMatchsPerdus = $nbMatchsPerdus;
-
         return $this;
     }
 
-    public function getUser(): ?User
-    {
+    public function getUser(): ?User {
         return $this->user;
+    }
+
+    public function setUser(?User $user): self {
+        $this->user = $user;
+        return $this;
+    }
+
+    public function getPartie(): ?Partie {
+        return $this->partie;
+    }
+
+    public function setPartie(?Partie $partie): self {
+        $this->partie = $partie;
+        return $this;
     }
 }
