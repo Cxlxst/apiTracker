6f65cb1949f5cd9fa20d6d26f1694552730fb518
MICLEA Christian <66925033+MicleaChristian@users.noreply.github.com>
2023-12-06 14:46:12 +0100

    add relations + logo et folder assets

--- tiltTracker/src/Entity/User.php
+++ tiltTracker/src/Entity/User.php
@@ -2,8 +2,10 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User
@@ -25,56 +27,75 @@ class User
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $statut = null;
 
-    public function getId(): ?int
-    {
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: UserMatch::class, cascade: ['persist', 'remove'])]
+    private Collection $userMatches;
+
+    public function __construct() {
+        $this->userMatches = new ArrayCollection();
+    }
+
+    public function getId(): ?int {
         return $this->id;
     }
 
-    public function getPseudo(): ?string
-    {
+    public function getPseudo(): ?string {
         return $this->pseudo;
     }
 
-    public function setPseudo(?string $pseudo): static
-    {
+    public function setPseudo(?string $pseudo): self {
         $this->pseudo = $pseudo;
-
         return $this;
     }
 
-    public function getNiveauCompte(): ?int
-    {
+    public function getNiveauCompte(): ?int {
         return $this->niveau_compte;
     }
 
-    public function setNiveauCompte(?int $niveau_compte): static
-    {
+    public function setNiveauCompte(?int $niveau_compte): self {
         $this->niveau_compte = $niveau_compte;
-
         return $this;
     }
 
-    public function getNbMatch(): ?int
-    {
+    public function getNbMatch(): ?int {
         return $this->nbMatch;
     }
 
-    public function setNbMatch(?int $nbMatch): static
-    {
+    public function setNbMatch(?int $nbMatch): self {
         $this->nbMatch = $nbMatch;
-
         return $this;
     }
 
-    public function getStatut(): ?string
-    {
+    public function getStatut(): ?string {
         return $this->statut;
     }
 
-    public function setStatut(?string $statut): static
-    {
+    public function setStatut(?string $statut): self {
         $this->statut = $statut;
+        return $this;
+    }
 
+    /**
+     * @return Collection|UserMatch[]
+     */
+    public function getUserMatches(): Collection {
+        return $this->userMatches;
+    }
+
+    public function addUserMatch(UserMatch $userMatch): self {
+        if (!$this->userMatches->contains($userMatch)) {
+            $this->userMatches[] = $userMatch;
+            $userMatch->setUser($this);
+        }
+        return $this;
+    }
+
+    public function removeUserMatch(UserMatch $userMatch): self {
+        if ($this->userMatches->removeElement($userMatch)) {
+            // set the owning side to null (unless already changed)
+            if ($userMatch->getUser() === $this) {
+                $userMatch->setUser(null);
+            }
+        }
         return $this;
     }
 }
