ba0ea18bcae60f32bd3721847c1334f7e555b9a4
lucastreille <lucas.treille1.0@gmail.com>
2024-01-11 10:32:51 +0100

    Update page connexion, userss, and add api call create account

--- tiltTracker/public/js/appelApi.js
+++ tiltTracker/public/js/appelApi.js
@@ -29,7 +29,7 @@ if (apiOption[0] === 'getListe') { //Récupération de la liste de tous les joue
     console.error('Erreur lors de la requête:', error);
   });
 
-   
+
 }
 else if (apiOption[0] === 'getSpecificAccount') //Récupération d'une fiche user --- apiOption : [0]:type de vérification - [1]: idUser
 {
@@ -97,7 +97,7 @@ else if (apiOption[0] === 'updateStatut') { //Changement du statut d'un user ---
     });
 
 }
-else if (apiOption[0] === 'verificationConnexion') { // système de vérirification des identifiant de connexion d'un user --- apiOption : [0]:type de vérification - [1]: pseudo - [2]: motDePasse 
+else if (apiOption[0] === 'verificationConnexion') { // système de vérirification des identifiant de connexion d'un user --- apiOption : [0]:type de vérification - [1]: pseudo - [2]: motDePasse
 
 
   console.log("Vérification de la connexion")
@@ -125,7 +125,7 @@ else if (apiOption[0] === 'verificationConnexion') { // système de véririficat
   .catch(error => {
     console.error('Erreur lors de la requête:', error);
   });
-  
+
 
 }
 else if (apiOption[0] === 'rechercheUser') { // système de recherche de user --- apiOption : [0]:type de vérification - [1]: recherche
@@ -155,7 +155,140 @@ else if (apiOption[0] === 'rechercheUser') { // système de recherche de user --
   // .catch(error => {
   //   console.error('Erreur lors de la requête:', error);
   // });
-  
+
+
+}
+else if (apiOption[0] === 'suppressionUser') {
+
+  console.log("Supression d'un membre")
+
+  fetch("http://127.0.0.1:8080/api/userss/" + apiOption[1], {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/ld+json',
+      'Content-Type': 'application/json',
+    },
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('La requête a échoué.');
+    }
+    console.log('Suppression réussie.');
+  })
+  .catch(error => {
+    console.error('Erreur lors de la requête DELETE:', error);
+  });
+
+
+
+}
+else if (apiOption[0] === 'ajoutMatchToUser') {
+
+  console.log("Insertion d'un match pour un joueur ")
+
+  //Insertion d'un match lié à un utilisateur avec récupération de l'id de liaison (match_user)
+  fetch("http://127.0.0.1:8080/api/match_users", {
+    method: 'POST',
+    headers: {
+      'accept': 'application/ld+json',
+      'Content-Type': 'application/ld+json'
+    },
+    body: JSON.stringify({
+      userId: '/api/userss/'+apiOption[1],
+      matchId: Number(apiOption[2]),
+      win: apiOption[3] === 'true' ? true : false
+    })
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('La requête inséré match a échoué.');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // data contient la réponse JSON de la requête
+    apiData = data;
+    console.log(apiData)
+    console.log("ID inséré :", apiData.id); 
+    var lastIdInsert = apiData.id
+
+
+    //Requête d'insertion dans user_match_stat
+    fetch("http://127.0.0.1:8080/api/user_match_stats", {
+      method: 'POST',
+      headers: {
+        'accept': 'application/ld+json',
+        'Content-Type': 'application/ld+json'
+      },
+      body: JSON.stringify({
+        userMatch: '/api/match_users/'+lastIdInsert,
+        killed: Number(apiOption[4]),
+        death:Number(apiOption[5]),
+        assist:Number(apiOption[6])
+      })
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('La requête inséré match a échoué.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // data contient la réponse JSON de la requête
+      apiData = data;
+      console.log(apiData)
+        
+    })
+    .catch(error => {
+      console.error('Erreur lors de la requête:', error);
+    });
+
+
+
+
+      
+  })
+  .catch(error => {
+    console.error('Erreur lors de la requête:', error);
+  });
+
+
+}
+else if (apiOption[0] === 'createAccount') {
+
+  console.log("Création d'un compte")
+
+  //Insertion d'un match lié à un utilisateur avec récupération de l'id de liaison
+  fetch("http://127.0.0.1:8080/api/userss", {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/ld+json',
+      'Content-Type': 'application/ld+json',
+    },
+    body: JSON.stringify({
+      "pseudo": apiOption[1],
+      "statut": apiOption[2],
+      "nbMatch": Number(apiOption[3]),
+      "mdp": apiOption[4]
+    }),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('La requête inséré compte a échoué.');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // data contient la réponse JSON de la requête
+    apiData = data;
+    console.log(apiData)
+    console.log("ID inséré :", apiData.id); // Assurez-vous que la propriété de l'ID est correcte
+    // miseEnPlaceVisionnageFicheJoueur(apiData);
+  })
+  .catch(error => {
+    console.error('Erreur lors de la requête:', error);
+  });
+
 
 }
 else
